c7c9e6eff59c0c97825ae3656b98abbcb9cc3b8a
Lucas Sacheto <lsacheto@katalystadvantage.com>
2023-10-27 09:43:07 -0400

    version working in progress

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,28 +1,71 @@
 
-import { useState } from "react";
-import "../tabs-config"
+import { createElement, useState, useEffect } from "react";
 
-const Tabs = ({TabName}) => {
+const Tabs = ({TabName, TabNum}) => {
 
-    const [tabContentName, setTabContentName] = useState("")
 
-    function showTabContent(e){
-        const getContentTab = document.getElementById(e.target.dataset.id)
-        console.log(getContentTab);
+    useEffect( () => {
+        
+        console.log(TabNum);
+        const getBody = document.getElementById("tabs-nav")
 
-        getContentTab.style.display = "block"
+        for (let i = 0; i < TabNum; i++) {
+            let contentObj = {}
+            let tabDiv = document.createElement("button");
+            tabDiv.className = "tab-"+TabName+i;
+            tabDiv.innerHTML = "tab-"+TabName+i
+            tabDiv.onclick = () => {
+                addClick(TabName+i)
+            }
+            //getBody.append(tabDiv)
+            contentObj[i] = tabDiv
+            getBody.append(contentObj[i])
+            setTabContent(current => [...current, contentObj[i].className]);
+
+        }
+
+        function addClick(e){
+            console.log("addClick "+e);
+            const getTabContent =  document.getElementById("tab-"+e);
+            const getAllTabContent = document.getElementsByClassName("tab-content")
+            
+            for (let i = 0; i < getAllTabContent.length; i++) {
+                getAllTabContent[i].style.display = "none"
+            }
+            getTabContent.style.display = "block"
+
+        }
+        
+        const getContentOutside = document.getElementById("tabs-nav")
+        console.log(getContentOutside);
+
+    },[])
+
+    
+    
+    const [tabContent, setTabContent] = useState([])
+
+    function TabContentCreation({name, className}){
+        return createElement("div", {id: name, className: className, style: {display: 'none'}}, "you have to create "+name+".js file inside /tabs folder")
+        //const createElementTest = createElement("div",{className: 'btnTabs', name: name, style: '', key: name}, name)
+        //console.log(createElementTest);
     }
-
+   
     return (
         <div className="tabsWrapper">
-            <div className="tabs-nav">
-                <ul>
-                    <li>
-                        <button onClick={showTabContent} data-id={TabName}>{TabName}</button>
-                    </li>
-                </ul>
+            <div id="tabs-nav" className="tabs-nav">
+                
+                {console.log("tabContent: "+tabContent)}
+                
+                {/* <button onClick={showTabContent} data-id={TabName}>{TabName}</button> */}
             </div>
-        </div>       
+            <>
+            { 
+                tabContent.map( (i) => createElement(TabContentCreation, {name: i, key: i, className: "tab-content"}) )
+            }
+            
+            </>
+        </div>
     )
 }
 
